e39a2610308452e2002c736ba0f9522dd21898aa
JaviMY <javier@masteryield.com>
2021-09-19 14:57:01 +0200

    kjh

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get();
+//Route::get();
 
 Route::get('test', function () {
     return "Funciona la api";
