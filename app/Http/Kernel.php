deaf260e6244a5c498c70a8709988280a9f36c1f
JaviMY <javier@masteryield.com>
2021-09-20 23:26:48 +0200

    sscf

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,11 +14,10 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \App\Http\Middleware\Cors::class,
+        //\App\Http\Middleware\Cors::class,
         \App\Http\Middleware\TrustHosts::class,
-        // \Fruitcake\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
-        // \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
