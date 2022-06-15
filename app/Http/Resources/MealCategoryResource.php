baf6423041c6c2d1d158b2504f2bc325289c03f9
javimasteryield <javier@masteryield.com>
2022-06-15 23:02:47 +0200

    .

--- app/Http/Resources/MealCategoryResource.php
+++ app/Http/Resources/MealCategoryResource.php
@@ -9,13 +9,14 @@ class MealCategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'         => $this->id,
-            'name'       => $this->name,
-            'category'   => $this->category ? new CategoryResource( $this->category ) : [],
-            'difficulty' => $this->difficulty,
-            'minutes'    => $this->minutes,
-            'kalories'   => $this->kalories,
-            'recipe'     => $this->recipe,
+            'id'          => $this->id,
+            'name'        => $this->name,
+            'category'    => $this->category ? new CategoryResource( $this->category ) : [],
+            'difficulty'  => $this->difficulty,
+            'minutes'     => $this->minutes,
+            'kalories'    => $this->kalories,
+            'recipe'      => $this->recipe,
+            'description' => $this->description,
         ];
     }
 }
